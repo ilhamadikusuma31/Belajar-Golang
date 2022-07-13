98ce1faf85561659d02a4164e980006544b084ef
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-07-14 00:06:51 +0700

    ngubah readme

--- dasar/5.variabel/main.go
+++ dasar/5.variabel/main.go
@@ -3,7 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ini string harus pake kutip dua")
-	fmt.Println(len("ilhamadi"))
-	fmt.Println("ilhamadi"[3])
+	fmt.Println("string")
+	fmt.Println("")
+	var namaku string
+	namaku = "ilhamadi"
+	fmt.Println(namaku)
+	namaku = "ADI"
+	fmt.Println(namaku)
+	fmt.Println("")
+	fmt.Println("kita bisa melakukan inisialisasi tanpa deklarasi")
+	negara := "indonesia"
+	fmt.Println(negara)
+
+	var (
+		nama_depan    = "ilham"
+		nama_belakang = "adikusuma"
+	)
+
+	fmt.Println(nama_depan)
+	fmt.Println(nama_belakang)
 }
