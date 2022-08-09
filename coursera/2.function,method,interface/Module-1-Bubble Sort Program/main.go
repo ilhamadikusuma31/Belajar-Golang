1d4f847c7132f6b39217acafd1bd1ee01d8d7d13
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-09 13:58:07 +0700

    bubble sort

--- coursera/2.function,method,interface/Module-1-Bubble Sort Program/main.go
+++ coursera/2.function,method,interface/Module-1-Bubble Sort Program/main.go
@@ -4,17 +4,22 @@ import "fmt"
 
 //nb: slice di golang itu sudah memakai prinsip pointer jadi gausah pake tanda & *
 
+func Swap(arr []int, index int) {
+	if arr[index] > arr[index+1] {
+		arr[index], arr[index+1] = arr[index+1], arr[index]
+	}
+
+}
+
 func BubbleSort(array []int) []int {
 	for i := 0; i < len(array)-1; i++ {
 		for j := 0; j < len(array)-i-1; j++ {
-			if array[j] > array[j+1] {
-				array[j], array[j+1] = array[j+1], array[j]
-			}
+			Swap(array, j)
 		}
 	}
 	return array
 }
 func main() {
-	array := []int{11, 14, 3, 8, 18, 17, 43}
+	array := []int{1, 23, 5, 43, 234, 402, 0, 65, 91, 90}
 	fmt.Println(BubbleSort(array))
 }
